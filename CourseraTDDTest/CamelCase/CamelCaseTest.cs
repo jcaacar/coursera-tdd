6f6c1fc42f33fb3cb3d52a77645f3575e19914d6
José Carlos Albuquerque de Araújo <jcaacar@gmail.com>
2020-04-13 21:45:31 -0300

    change CamelCaseTest method's name to feature tested

--- CourseraTDDTest/CamelCase/CamelCaseTest.cs
+++ CourseraTDDTest/CamelCase/CamelCaseTest.cs
@@ -18,94 +18,100 @@ namespace CourseraTDDTEST.CamelCase
         }
 
         [Test]
-        public void WithoutUpperSingleTest()
+        public void OnlyOneWordLowercase()
         {
             var result = camelCaseSolution.Convert("nome");
 
             var expected = new List<string> { "nome" };
 
-            Assert.AreEqual(result.Count, 1);
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void WithUpperSingleTest()
+        public void OnlyOneWordWithFirstUppercase()
         {
             var result = camelCaseSolution.Convert("Nome");
 
             var expected = new List<string> { "nome" };
-            Assert.AreEqual(1, result.Count);
+
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void StartWithoutUpperCompoundTest()
+        public void CompoundWithFirstLowercase()
         {
             var result = camelCaseSolution.Convert("nomeComposto");
 
             var expected = new List<string> { "nome", "composto" };
-            Assert.AreEqual(2, result.Count);
+
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void StartWithUpperCompoundTest()
+        public void CompoundWithFirstUppercase()
         {
             var result = camelCaseSolution.Convert("NomeComposto");
 
             var expected = new List<string> { "nome", "composto" };
-            Assert.AreEqual(2, result.Count);
+
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void AcronymSingleTest()
+        public void OnlyAcronym()
         {
             var result = camelCaseSolution.Convert("CPF");
 
             var expected = new List<string> { "CPF" };
-            Assert.AreEqual(1, result.Count);
+
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void AcronymInTheEndTest()
+        public void EndWithAnAcronym()
         {
             var result = camelCaseSolution.Convert("numeroCPF");
 
             var expected = new List<string> { "numero", "CPF" };
-            Assert.AreEqual(2, result.Count);
+
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void AcronymInTheMiddleTest()
+        public void AcronymInTheMiddle()
         {
             var result = camelCaseSolution.Convert("numeroCPFcontribuinte");
 
             var expected = new List<string> { "numero", "CPF", "contribuinte" };
-            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void NumbersInTheMiddleTest()
+        public void NumbersInTheMiddle()
         {
             var result = camelCaseSolution.Convert("recupera10primeiros");
 
             var expected = new List<string> { "recupera", "10", "primeiros" };
-            Assert.AreEqual(3, result.Count);
+
+            Assert.AreEqual(expected.Count, result.Count);
             Assert.IsTrue(expected.All(i => result.Contains(i)));
         }
 
         [Test]
-        public void StartWithNumberTest()
+        public void CantStartWithNumber()
         {
             Assert.Throws<ArgumentException>(() => camelCaseSolution.Convert("10Primeiros"));
         }
 
         [Test]
-        public void WithSpecialCharacerTest()
+        public void CantSpecialCharacter()
         {
             Assert.Throws<ArgumentException>(() => camelCaseSolution.Convert("nome#Composto"));
         }
