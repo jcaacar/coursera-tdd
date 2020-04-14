3c32edd28d6918b6e42e28b9b53d6352691baf44
José Carlos Albuquerque de Araújo <jcaacar@gmail.com>
2020-04-13 22:14:38 -0300

    improve CamelCase's assertion, rename namespace alias and instance name.

--- CourseraTDDTest/CamelCase/CamelCaseTest.cs
+++ CourseraTDDTest/CamelCase/CamelCaseTest.cs
@@ -2,128 +2,121 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using camel = CourseraTDD.CamelCase;
+using cc = CourseraTDD.CamelCase;
 
 namespace CourseraTDDTEST.CamelCase
 {
     [TestFixture]
     public class CamelCaseTest
     {
-        private camel.CamelCase camelCaseSolution;
+        private cc.CamelCase camelCase;
 
         [OneTimeSetUp]
         public void Setup()
         {
-            camelCaseSolution = new camel.CamelCase();
+            camelCase = new cc.CamelCase();
         }
 
         [Test]
         public void OnlyOneWordLowercase()
         {
-            var result = camelCaseSolution.Convert("nome");
+            var result = camelCase.Convert("nome");
 
             var expected = new List<string> { "nome" };
 
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void OnlyOneWordWithFirstUppercase()
         {
-            var result = camelCaseSolution.Convert("Nome");
+            var result = camelCase.Convert("Nome");
 
             var expected = new List<string> { "nome" };
 
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void CompoundWithFirstLowercase()
         {
-            var result = camelCaseSolution.Convert("nomeComposto");
+            var result = camelCase.Convert("nomeComposto");
 
             var expected = new List<string> { "nome", "composto" };
 
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void CompoundWithFirstUppercase()
         {
-            var result = camelCaseSolution.Convert("NomeComposto");
+            var result = camelCase.Convert("NomeComposto");
 
             var expected = new List<string> { "nome", "composto" };
 
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void OnlyAcronym()
         {
-            var result = camelCaseSolution.Convert("CPF");
+            var result = camelCase.Convert("CPF");
 
             var expected = new List<string> { "CPF" };
 
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void EndWithAnAcronym()
         {
-            var result = camelCaseSolution.Convert("numeroCPF");
+            var result = camelCase.Convert("numeroCPF");
 
             var expected = new List<string> { "numero", "CPF" };
 
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void AcronymInTheMiddle()
         {
-            var result = camelCaseSolution.Convert("numeroCPFcontribuinte");
+            var result = camelCase.Convert("numeroCPFcontribuinte");
 
             var expected = new List<string> { "numero", "CPF", "contribuinte" };
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void ManyAcronyms()
         {
-            var result = camelCaseSolution.Convert("numeroCPFeRGcontribuintePF");
+            var result = camelCase.Convert("numeroCPFeRGcontribuintePF");
 
             var expected = new List<string> { "numero", "CPF", "e", "RG", "contribuinte", "PF" };
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void NumbersInTheMiddle()
         {
-            var result = camelCaseSolution.Convert("recupera10primeiros");
+            var result = camelCase.Convert("recupera10primeiros");
 
-            var expected = new List<string> { "recupera", "10", "primeiros" };
+            var expected = new List<string> { "recupera", "10", "primeiros"};
 
-            Assert.AreEqual(expected.Count, result.Count);
-            Assert.IsTrue(expected.All(i => result.Contains(i)));
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void CantStartWithNumber()
         {
-            Assert.Throws<ArgumentException>(() => camelCaseSolution.Convert("10Primeiros"));
+            Assert.Throws<ArgumentException>(() => camelCase.Convert("10Primeiros"));
         }
 
         [Test]
         public void CantSpecialCharacter()
         {
-            Assert.Throws<ArgumentException>(() => camelCaseSolution.Convert("nome#Composto"));
+            Assert.Throws<ArgumentException>(() => camelCase.Convert("nome#Composto"));
         }
     }
 }
