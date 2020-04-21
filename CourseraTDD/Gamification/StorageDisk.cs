293be535522cb732174c3dad8df8a88de0135254
José Carlos Albuquerque de Araújo <jcaacar@gmail.com>
2020-04-21 01:10:57 -0300

    gamification exercise implemented Storage Disk

---
+++ CourseraTDD/Gamification/StorageDisk.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CourseraTDD.Gamification
+{
+    public class StorageDisk
+    {
+        #region Cons
+
+        private const string RootFolder = "storage";
+
+        #endregion
+
+        #region Props
+
+        public string FolderPath { get; }
+
+        #endregion
+
+        #region Ctor
+
+        public StorageDisk(string path)
+        {
+            FolderPath = Path.Combine(path, RootFolder);
+        }
+
+        #endregion
+
+        #region Public
+
+        public void Save(string user, int points, string pointType)
+        {
+            var userFolderPath = CreateUserFolder(user);
+            var pointTypePath = GetPointFilePath(userFolderPath, pointType);
+
+            points += ReadCurrentPoints(pointTypePath);
+
+            File.WriteAllText(pointTypePath, points.ToString());
+        }
+
+        public int GetTotalPoints(string user, string pointType)
+        {
+            var userFolderPath = GetUserPath(user);
+
+            var pointTypePath = GetPointFilePath(userFolderPath, pointType);
+
+            if (File.Exists(pointTypePath))
+                return int.Parse(File.ReadAllText(pointTypePath));
+
+            return 0;
+        }
+
+        public List<string> GetAllUserWithPoints()
+        {
+            var result = new List<string>();
+
+            if (!Directory.Exists(FolderPath))
+                return result;
+
+            foreach (var userFolder in Directory.GetDirectories(FolderPath))
+            {
+                if (Directory.GetFiles(userFolder).Length > 0)
+                {
+                    result.Add(Path.GetFileName(userFolder));
+                }
+            }
+
+            return result;
+        }
+
+        public List<string> GetPointsType(string user)
+        {
+            var userPath = GetUserPath(user);
+
+            if (!Directory.Exists(userPath))
+                return new List<string>();
+
+            return Directory.GetFiles(userPath).Select(f => Path.GetFileName(f)).ToList();
+        }
+
+        public void Clear()
+        {
+            if (Directory.Exists(FolderPath))
+            {
+                Directory.Delete(FolderPath, true);
+            }
+        }
+
+        #endregion
+
+        #region Private
+
+        private string CreateUserFolder(string user)
+        {
+            return Directory.CreateDirectory(GetUserPath(user)).FullName;
+        }
+
+        private string GetUserPath(string user)
+        {
+            return Path.Combine(FolderPath, user);
+        }
+
+        private string GetPointFilePath(string userPath, string pointType)
+        {
+            return Path.Combine(userPath, pointType);
+        }
+
+        private int ReadCurrentPoints(string pointTypePath)
+        {
+            if (File.Exists(pointTypePath))
+            {
+                var currentPoints = File.ReadAllText(pointTypePath);
+                if (int.TryParse(currentPoints, out int parsedPoints))
+                {
+                    return parsedPoints;
+                }
+            }
+
+            return 0;
+        }
+
+        #endregion
+
+    }
+}
---
+++ CourseraTDDTest/Gamification/StorageDiskTest.cs
@@ -0,0 +1,85 @@
+﻿using CourseraTDD.Gamification;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CourseraTDDTEST.Gamification
+{
+    [TestFixture]
+    public class StorageDiskTest
+    {
+        private string FolderPath => Directory.GetCurrentDirectory();
+
+        private readonly string userZeca = "zeca";
+        private readonly string userJuju = "juju";
+
+        private readonly int points = 10;
+
+        private readonly string starType = "star";
+        private readonly string medalType = "medal";
+        private readonly string coinType = "coin";
+
+        private StorageDisk storage;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            storage = new StorageDisk(FolderPath);
+        }
+
+
+        [Test]
+        public void SaveUserSingle()
+        {
+            storage.Save(userZeca, points, starType);
+
+            var result = storage.GetTotalPoints(userZeca, starType);
+
+            Assert.AreEqual(points, result);
+        }
+
+        [Test]
+        public void SaveTheSameUserTwoTimes()
+        {
+            storage.Save(userZeca, points, starType);
+            storage.Save(userZeca, points, starType);
+
+            var result = storage.GetTotalPoints(userZeca, starType);
+
+            Assert.AreEqual(points * 2, result);
+        }
+
+        [Test]
+        public void GetAllUserThatReceivedPoints()
+        {
+            var expected = new List<string> { userZeca, userJuju };
+
+            storage.Save(userZeca, points, starType);
+            storage.Save(userJuju, points, medalType);
+
+            var result = storage.GetAllUserWithPoints();
+
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+
+        [Test]
+        public void GetPointsTypeReceivedFromUser()
+        {
+            var expected = new List<string> { starType, medalType, coinType };
+
+            storage.Save(userZeca, points, starType);
+            storage.Save(userZeca, points, medalType);
+            storage.Save(userZeca, points, coinType);
+
+            var result = storage.GetPointsType(userZeca);
+
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            storage.Clear();
+        }
+    }
+}
